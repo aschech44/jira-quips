3e404b274fe99353a5f284be8eb7228cd32ed73f
Luke Halliwell <Luke.J.Halliwell@googlemail.com>
2010-04-22 08:21:51 +0100

    Plugin skeleton as generated by Atlassian tools

---
+++ src/main/java/com/jiraquips/MyPlugin.java
@@ -0,0 +1,5 @@
+package com.jiraquips;
+
+public class MyPlugin
+{
+}
---
+++ src/test/java/com/jiraquips/MyPluginTest.java
@@ -0,0 +1,11 @@
+package com.jiraquips;
+
+import org.junit.Test;
+
+public class MyPluginTest
+{
+    @Test
+    public void testSomething()
+    {
+    }
+}
